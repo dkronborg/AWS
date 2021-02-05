8b723a0e6cb59b805f27036bd5efd2976d69be97
git-dkronborg <dan.kronborg@protonmail.com>
2021-02-05 13:26:45 +0100

    added nginxinstall script

---
+++ nginsxinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#install nginx
+sudo apt install nginx -y
+#check out website, code 200 ok
+#wget 127.0.0.1 -qO-
+#echo \<html\>dan\</html\> | sudo tee /var/www/html/index.nginx-debian.html
+#replace index file
+#sudo cp index.html /var/www/html/index.nginx-debian.html
+#look at website
+wget 127.0.0.1 -qO-
+
+#moitor nginx process
+top -p `pidof -s nginx`
+
+#look at access log
+tail -f /var/log/nginx/access.log 
