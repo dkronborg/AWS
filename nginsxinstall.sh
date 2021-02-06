38b9ffe2b90ae5b025e098715882018204c4f27c
git-dkronborg <dan.kronborg@protonmail.com>
2021-02-06 20:27:27 +0100

    i dno more updates i guess

--- nginsxinstall.sh
+++ nginsxinstall.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+#curl needs to be installed
+#sudo apt install curl -y
+#bash <(curl -Ls https://raw.githubusercontent.com/dkronborg/AWS/main/nginsxinstall.sh)
+#cat <(curl -Ls https://raw.githubusercontent.com/dkronborg/AWS/main/index.html) | sudo tee /var/www/html/index.nginx-debian.html
+
 #install nginx
 sudo apt install nginx -y
 #check out website, code 200 ok
-#wget 127.0.0.1 -qO-
-#echo \<html\>dan\</html\> | sudo tee /var/www/html/index.nginx-debian.html
-#cat index.html | sudo tee /var/www/html/index.nginx-debian.html
-#replace index file
-#sudo cp index.html /var/www/html/index.nginx-debian.html
-#look at website
 wget 127.0.0.1 -qO-
 
 #moitor nginx process
