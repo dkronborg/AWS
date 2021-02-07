e8c574496a33d71b409b3848d6647160c39ba7f1
git-dkronborg <dan.kronborg@protonmail.com>
2021-02-07 20:53:56 +0100

    replace index in script

--- nginsxinstall.sh
+++ nginsxinstall.sh
@@ -7,6 +7,9 @@
 
 #install nginx
 sudo apt install nginx -y
+#replace index.html
+cat <(curl -Ls https://raw.githubusercontent.com/dkronborg/AWS/main/index.html) | sudo tee /var/www/html/index.nginx-debian.html
+
 #check out website, code 200 ok
 wget 127.0.0.1 -qO-
 
@@ -15,3 +18,19 @@ wget 127.0.0.1 -qO-
 
 #look at access log
 #tail -f /var/log/nginx/access.log 
+
+#add new hard drive in virtual box
+#check to see which drive has been added, look at the size to determine which one
+#dmesg | grep sd
+#create file system (sdb is my drive from previous command)
+#sudo mkfs -t ext4 /dev/sdb
+#create directory to mount to
+#sudo mkdir /media/newhd
+#mount
+#sudo mount /dev/sdb /media/newhd
+#make sure its added on startup
+#sudo nano /etc/fstab
+#add following line
+#/dev/sdb        /media/newhd    ext4    defaults        0       2
+
+
